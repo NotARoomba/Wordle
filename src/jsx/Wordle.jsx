3db32c23c6597217d1d355eaa04bf6c4974b33b5
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-07-16 00:52:09 -0500

    a

--- src/jsx/Wordle.jsx
+++ src/jsx/Wordle.jsx
@@ -1,19 +1,32 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 
 import Word from './Word.jsx'
 
 import '../css/App.css'
 
 export default function Wordle() {
-  
+  useEffect(() => {
+    function handleKeyDown(e) {
+      console.log(e.key);
+      for (let child in $(".words-box")[0].children) {
+        console.log($(".words-box")[0].children)
+        if (child.textContent.length != 5) {
+          child.textContent += e.key;
+        }
+      }
+    }
+    document.addEventListener('keydown', handleKeyDown);
+    return function cleanup() {
+      document.removeEventListener('keydown', handleKeyDown);
+    }
+  }, []);
   return (
     <>
       <div className="top-text">
         <p>Wordle</p>
       </div>
-      <div className="words-box"   onKeyDown={e => console.log('onKeyDown')}
-  onKeyUp={e => console.log('onKeyUp')}>
-        <Word word={""}/>
+      <div className="words-box">
+        <Word word={"aaaa"}/>
         <Word word={""}/>
         <Word word={""}/>
         <Word word={""}/>
