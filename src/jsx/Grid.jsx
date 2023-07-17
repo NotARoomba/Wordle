c0564cfeeab108e21cd641c2439c0c87d9995d9e
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-07-16 19:43:26 -0500

    aa useState not working

--- src/jsx/Grid.jsx
+++ src/jsx/Grid.jsx
@@ -4,14 +4,12 @@ import { MAX_GUESSES } from './Constants'
 
 export default function Grid({guesses, currentGuess}) {
     const empty = guesses.length < MAX_GUESSES ? Array(MAX_GUESSES - guesses.length).fill("") : [];
-    console.log(empty)
   return (<>
     {guesses.map((g) => {
         <Word status={"complete"} word={g}/>
     })}
     <Word status={"inprogress"} word={currentGuess}/>
-    {empty.map((g) => {
-        <Word status={"uncomplete"} word={g}/>
-    })}
+    {empty.map((g, i) => 
+        (<Word key={i} status={"uncomplete"} word={g}/>))}
   </>)
 }
--- src/jsx/Wordle.jsx
+++ src/jsx/Wordle.jsx
@@ -2,18 +2,23 @@ import { useEffect, useState } from 'react'
 
 import Grid from "./Grid.jsx"
 import Keyboard from './Keyboard.jsx';
+import { WORD_SIZE } from './Constants.jsx';
 
 import '../css/App.css'
 
 export default function Wordle() {
-  const answer = "fires"
   const [currentGuess, setCurrentGuess] = useState("")
   const [guesses, setGuesses] = useState(Array())
   // const [currentGuess, setGuessCount] = useState(0)
   // let [guesses, setGuess] = useState(Array(maxGuess).fill(""))
-  
+  useEffect(() => {
+  }, [])
   const onChar = (value) => {
-    console.log(value)
+    if (currentGuess.length+1 <= WORD_SIZE) {
+      console.log(value.toString())
+      setCurrentGuess(`${currentGuess}${value.toString()}`)
+      console.log(`${currentGuess}${value}`)
+    }
   }
   const onDelete = (value) => {
     console.log("DELETE")
