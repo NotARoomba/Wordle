2f1478c8bd07565c72c303289c806492b9d0ca70
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-07-17 16:01:14 -0500

    Revert "wordle"
    This reverts commit 6fb8f68381d120e4caed6d633616e147d6e165cd.

--- src/jsx/Wordle.jsx
+++ src/jsx/Wordle.jsx
@@ -1,32 +1,19 @@
-import { useEffect } from 'react'
+import { useState } from 'react'
 
 import Word from './Word.jsx'
 
 import '../css/App.css'
 
 export default function Wordle() {
-  useEffect(() => {
-    function handleKeyDown(e) {
-      console.log(e.key);
-      for (let child in $(".words-box")[0].children) {
-        console.log($(".words-box")[0].children)
-        if (child.word.length != 5) {
-          chold.word += e.key;
-        }
-      }
-    }
-    document.addEventListener('keydown', handleKeyDown);
-    return function cleanup() {
-      document.removeEventListener('keydown', handleKeyDown);
-    }
-  }, []);
+  
   return (
     <>
       <div className="top-text">
         <p>Wordle</p>
       </div>
-      <div className="words-box">
-        <Word word={"aaaaaa"}/>
+      <div className="words-box"   onKeyDown={e => console.log('onKeyDown')}
+  onKeyUp={e => console.log('onKeyUp')}>
+        <Word word={""}/>
         <Word word={""}/>
         <Word word={""}/>
         <Word word={""}/>
