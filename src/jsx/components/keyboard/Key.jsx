fca594034b29cefaed3b0a55827a4760cae350c4
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-07-28 22:04:59 -0500

    more responsive

--- src/jsx/components/keyboard/Key.jsx
+++ src/jsx/components/keyboard/Key.jsx
@@ -3,6 +3,6 @@ import { WORD_SIZE, REVEAL_DELAY } from "../../Constants";
 
 export default function Key({onChar, value, type, reveal}) {
     return (<div className="key" onClick={() => (onChar(value))} id={reveal?(reveal?"key-reveal-"+(type==undefined?0:type):""):"key-"+(type==undefined?0:type)} style={{animationDelay: (REVEAL_DELAY * WORD_SIZE) + 's' }}>
-        {value=="DELETE"?<img src="/assets/delete.svg"/>:<p className={(reveal&&value!="ENTER"&&type!=undefined?"reveal":"")} style={{animationDelay: (REVEAL_DELAY * WORD_SIZE) + 's' }}>{value}</p>}
+        {value=="DELETE"?<img src="/assets/delete.svg"/>:value=="ENTER"?<img src="/assets/corner-down-left.svg"/>:<p className={(reveal&&value!="ENTER"&&type!=undefined?"reveal":"")} style={{animationDelay: (REVEAL_DELAY * WORD_SIZE) + 's' }}>{value}</p>}
     </div>)
 }
